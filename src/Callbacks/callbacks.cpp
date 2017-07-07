8e087d93ef43112d394f5abe34fbc0a4a4ec5457
Marcelino <marcelino.malmeidan@gmail.com>
2017-07-07 09:19:31 -0500

    added camera in tf tree

--- src/Callbacks/callbacks.cpp
+++ src/Callbacks/callbacks.cpp
@@ -17,16 +17,27 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr &msg){
 
 void tfCallback(const nav_msgs::Odometry::ConstPtr &msg){
 	static tf::TransformBroadcaster br;
-	tf::Transform transform;
-	transform.setOrigin(tf::Vector3(msg->pose.pose.position.x,
+	tf::Transform transformQuad, transformCamera;
+	transformQuad.setOrigin(tf::Vector3(msg->pose.pose.position.x,
 		                            msg->pose.pose.position.y,
 		                            msg->pose.pose.position.z) );
-  	transform.setRotation(tf::Quaternion(msg->pose.pose.orientation.x,
+  	transformQuad.setRotation(tf::Quaternion(msg->pose.pose.orientation.x,
   		                                 msg->pose.pose.orientation.y,
   		                                 msg->pose.pose.orientation.z,
   		                                 msg->pose.pose.orientation.w));
-  	// printf("tf called\n");
- 	br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "fcu", "quad"));
+  	transformCamera.setOrigin(tf::Vector3(0.46,0.0,0.0));
+
+  	Eigen::Matrix3d R;
+  	R <<  0.0,  0.0, 1.0,
+  	     -1.0,  0.0, 0.0,
+  	      0.0, -1.0, 0.0; 
+	geometry_msgs::Quaternion q = rot2quat(R);
+  	transformCamera.setRotation(tf::Quaternion(q.x, q.y, q.z, q.w));
+  	// transformCamera.setRotation(tf::Quaternion(0.0,sin(3.14/4.0),0.0,cos(3.14/4.0)));
+
+
+ 	br.sendTransform(tf::StampedTransform(transformQuad, ros::Time::now(), "fcu", "quad"));
+ 	br.sendTransform(tf::StampedTransform(transformCamera, ros::Time::now(), "quad", "camera"));
 }
 
 //Get data from joysticks using appropriate driver
